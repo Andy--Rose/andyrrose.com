487054525f6e82119b761d1abde8a14c0cb0d759
Andy Rose <andy.rose2588@gmail.com>
2016-09-12 13:21:03 -0600

    Bootstrapped header information

---
+++ scripts/bootstrap.js
@@ -0,0 +1,9 @@
+var head = document.getElementsByTagName("head")[0];
+head.innerHTML += '<meta charset="utf-8" />';
+head.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1" />';
+head.innerHTML += '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+head.innerHTML += '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>';
+head.innerHTML += '<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+head.innerHTML += '<link rel="stylesheet" type="text/css" href="css/styleSheet.css" />';
+head.innerHTML += '<link rel="icon" type="image/icon" href="http://www.andyrrose.com/icon.ico" />';
+head.innerHTML += '<link rel="shortcut icon" type="image/icon" href="http://www.andyrrose.com/icon.ico" />';
