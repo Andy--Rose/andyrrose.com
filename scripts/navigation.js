ee8539721c9f02a035176c8961affa3267e39aa9
Andy Rose <arose@medallia.com>
2016-09-16 12:44:26 -0600

    Spruce up the Home page
    * Profile pic and bg
    
    Moved about me contents out
    
    * Centered Nav
    
    * Moved Header
    
    * Body bg color
    
    * Set about page

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,12 +1,11 @@
-// TODO: When the page gets to the point when it scales virtically
 function setNavigation(page) {
-	var nav = '<nav class="navbar navbar-inverse navbar-fixed-top">';
+	var nav = '<nav class="navbar navbar-inverse">';
 	nav += '<div class="container-fluid">';
 	nav += '<div class="navbar-header">';
 	nav += '<a class="navbar-brand" href="Home.html">Andy Rose</a>';
 	nav += '</div>';
 	nav += '<ul class="nav navbar-nav">';
-	nav += '<li id="home"><a href="Home.html">Home</a></li>';
+	nav += '<li id="about"><a href="About.html">About</a></li>';
 	nav += '<li id="info"><a href="Info.html">Info/Skills</a></li>';
 	nav += '<li id="work"><a href="Work.html">Work History</a></li>';
 	nav += '<li id="projects"><a href="Projects.html">Projects</a></li>';
@@ -26,4 +25,11 @@ function setNavigation(page) {
 			item.className = "inactive";
 		}
 	});
+}
+
+function setHomeNav() {
+	setNavigation('home');
+	$(".navbar-brand")[0].remove();
+	$(".navbar").css("display", "inline-block");
+    $(".navbar").css("float", "none");
 }
