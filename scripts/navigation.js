ac56c3e2858ef2499879a84c7adb23ea4f4a650b
Andy Rose <andy.rose2588@gmail.com>
2016-09-16 17:03:32 -0600

    Fixed menu responsiveness
    Was not working right because it was being rendered after the DOM
    elements of the document were written.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,20 +1,7 @@
 function setNavigation(page) {
-	var nav = '<nav class="navbar navbar-inverse">';
-	nav += '<div class="container-fluid">';
-	nav += '<div class="navbar-header">';
-	nav += '<a class="navbar-brand" href="Home.html">Andy Rose</a>';
-	nav += '</div>';
-	nav += '<ul class="nav navbar-nav">';
-	nav += '<li id="about"><a href="About.html">About</a></li>';
-	nav += '<li id="info"><a href="Info.html">Info/Skills</a></li>';
-	nav += '<li id="work"><a href="Work.html">Work History</a></li>';
-	nav += '<li id="projects"><a href="Projects.html">Projects</a></li>';
-	nav += '<li id="papers"><a href="Papers.html">Papers</a></li>';
-	nav += '</ul>';
-	nav += '</div>';
-	nav += '</nav>';
+/* 	var nav = getNavigationHtml();
 
-	document.getElementById("navMenu").innerHTML = nav;
+	document.getElementById("navMenu").innerHTML = nav; */
 	
 	$(".nav").find("li").each(function () {
 		var item = $(this)[0];
@@ -25,6 +12,32 @@ function setNavigation(page) {
 			item.className = "inactive";
 		}
 	});
+}
+
+function getNavigationHtml() {
+	var nav = '<nav class="navbar navbar-default navbar-inverse" role="navigation">';
+	nav += '<div class="container-fluid" id="navfluid">';
+	nav += '<div class="navbar-header">';
+	nav += '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">';
+	nav += '<span class="sr-only">Toggle Navigation</span>';
+	nav += '<span class="icon-bar"></span>';
+	nav += '<span class="icon-bar"></span>';
+	nav += '<span class="icon-bar"></span>';
+	nav += '</button>';
+	nav += '<a class="navbar-brand" href="./Home.html">Andy Rose</a>';
+	nav += '</div>';
+	nav += '<div class="collapse navbar-collapse" id="navigationbar">';
+	nav += '<ul class="nav navbar-nav">';
+	nav += '<li id="about"><a href="./About.html">About</a></li>';
+	nav += '<li id="info"><a href="./Info.html">Info/Skills</a></li>';
+	nav += '<li id="work"><a href="./Work.html">Work History</a></li>';
+	nav += '<li id="projects"><a href="./Projects.html">Projects</a></li>';
+	nav += '<li id="papers"><a href="./Papers.html">Papers</a></li>';
+	nav += '</ul>';
+	nav += '</div>';
+	nav += '</div>';
+	nav += '</nav>';
+	return nav;
 }
 
 function setHomeNav() {
